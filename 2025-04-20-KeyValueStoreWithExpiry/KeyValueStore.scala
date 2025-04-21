6f7959090db5a3ae2a68b304e55327f35fe68562
Vicente Figueiredo <vicenteppfigueiredo@gmail.com>
2025-04-21 02:59:36 +0100

    Cleanup

--- 2025-04-20-KeyValueStoreWithExpiry/KeyValueStore.scala
+++ 2025-04-20-KeyValueStoreWithExpiry/KeyValueStore.scala
@@ -1,71 +1,53 @@
 import scala.io.StdIn.readLine
 import scala.util._
-import Common.KV
 
-object Common {
-  type KV = Map[String, Value]
-  val DefaultSaveFile: String = "save"
-}
+
 object KeyValueStore extends App {
-  def startup: KV = {
-    Map.empty
-  }
 
-  def checkExpired(kv: KV): KV = {
-    kv.map(kv => kv._1 -> kv._2.updateIfExpired)
-  }
+  iterate(KV.empty)
 
+  def iterate(kv: KV): Unit = {
+    print("> ")
+    val tryCmd = Parser.parseInput(readLine())
+
+    val modifiedKv = tryCmd match {
+      case Failure(_) =>
+        println("Unable to parse Instruction.")
+        kv
+      case Success(command) =>
+        command.process(kv.updateExpired)
+    }
+    iterate(modifiedKv)
+  }
+}
+
+object Parser {
   def parseInput(input: String): Try[Command] = {
-    def parseGetCommand(input: List[String]): Get = input match {
-      case "GET" :: key :: Nil => Get(key)
-      case _ => throw new RuntimeException("Unable to parse Input")
-    }
-
-    def parseSetCommand(input: List[String]): Set = input match {
-      case "SET" :: key :: value :: ttl :: Nil => Set(key, value, ttl.toInt)
-      case "SET" :: key :: value :: Nil => Set(key, value, -1)
-      case _ => throw new RuntimeException("Unable to parse Input")
-    }
-
-    def parseDelCommand(input: List[String]): Del = input match {
-      case "DEL" :: key :: Nil => Del(key)
-      case _ => throw new RuntimeException("Unable to parse Input")
-    }
-
     Try {
-      val parts = input.split(" ").toList
-      parts(0) match {
-        case Command.GET_IDENTIFIER => parseGetCommand(parts)
-        case Command.SET_IDENTIFIER => parseSetCommand(parts)
-        case Command.DEL_IDENTIFIER => parseDelCommand(parts)
-        case Command.QUIT_IDENTIFIER => QUIT
+      input.split(" ").toList match {
+        case CmdId.GET :: key :: Nil => Get(key)
+
+        case CmdId.SET :: key :: value :: ttl :: Nil => Set(key, value, ttl.toInt)
+        case CmdId.SET :: key :: value :: Nil => Set(key, value, -1)
+
+        case CmdId.DEL :: key :: Nil => Del(key)
+
+        case CmdId.QUIT :: Nil => QUIT
+        case _ => throw new RuntimeException("Unable to parse Input")
       }
     }
   }
+}
 
+object KV {
+  val empty: KV = KV(Map.empty)
+}
+case class KV(map: Map[String, Value]) {
+  def +(tup: (String, Value)): KV = KV(map + tup)
+  def -(key: String): KV = KV(map - key)
+  def get(key: String): Option[Value] = map.get(key)
 
-  def run(): Unit = {
-    def iterate(kv: KV): Unit = {
-
-      print("> ")
-      val input = readLine()
-      val tryCmd = parseInput(input)
-
-      val modifiedKv = tryCmd match {
-        case Failure(_) =>
-          println("Unable to parse Instruction.")
-          kv
-        case Success(command) =>
-          command.process(checkExpired(kv))
-      }
-
-      iterate(modifiedKv)
-    }
-
-    iterate(startup)
-  }
-  run()
-
+  def updateExpired: KV = KV(map.map(kv => kv._1 -> kv._2.updateIfExpired))
 }
 
 sealed trait Value {
@@ -90,11 +72,11 @@ case object Expired extends Value {
   override def updateIfExpired: Value = Expired
 }
 
-object Command {
-  val SET_IDENTIFIER = "SET"
-  val DEL_IDENTIFIER = "DEL"
-  val GET_IDENTIFIER = "GET"
-  val QUIT_IDENTIFIER = "QUIT"
+object CmdId {
+  val SET = "SET"
+  val DEL = "DEL"
+  val GET = "GET"
+  val QUIT = "QUIT"
 }
 sealed trait Command {
   def process(kv: KV): KV
